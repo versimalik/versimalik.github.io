de5dcd1f006be29aa7ad1bfd2bdd65045eab63e7
abulubabah <rezamalikdrive@gmail.com>
2022-04-20 13:40:22 +0700

    trying vuepress v.2

---
+++ assets/404.html.992eddff.js
@@ -0,0 +1,1 @@
+import{_}from"./app.33b24d23.js";const e={};function r(t,c){return null}var f=_(e,[["render",r],["__file","404.html.vue"]]);export{f as default};
---
+++ assets/about.html.d7e42930.js
@@ -0,0 +1,1 @@
+const t={key:"v-22a39d25",path:"/about.html",title:"ini halaman about",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{},filePathRelative:"about.md"};export{t as data};
