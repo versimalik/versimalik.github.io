4850aa048182b507e12561cc1c0bb34cf32302c3
abulubabah <rezamalikdrive@gmail.com>
2022-04-21 10:20:24 +0700

    deploy

---
+++ assets/404.html.c8028690.js
@@ -0,0 +1,1 @@
+import{_}from"./app.acf72cd9.js";const e={};function r(t,c){return null}var f=_(e,[["render",r],["__file","404.html.vue"]]);export{f as default};
---
+++ assets/about.html.ce5ffab5.js
@@ -0,0 +1,1 @@
+const t={key:"v-22a39d25",path:"/about.html",title:"",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{},filePathRelative:"about.md"};export{t as data};
