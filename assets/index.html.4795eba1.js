24705b17fdc17b124fc66dc8d8a579b008994bc1
abulubabah <rezamalikdrive@gmail.com>
2022-04-21 10:25:22 +0700

    deploy

---
+++ assets/index.html.4795eba1.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,c as t,a as i,b as n}from"./app.39225ee6.js";const o={},s={id:"ini-malik",tabindex:"-1"},c=i("a",{class:"header-anchor",href:"#ini-malik","aria-hidden":"true"},"#",-1),_=n(" Ini Malik"),r=[c,_];function d(h,l){return a(),t("h1",s,r)}var m=e(o,[["render",d],["__file","index.html.vue"]]);export{m as default};
