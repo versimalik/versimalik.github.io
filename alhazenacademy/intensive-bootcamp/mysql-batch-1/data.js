dfa198ac34450332591ce0c2e52cd87ef98cf143
Reza Malik <rezamalikdrive@gmail.com>
2024-04-29 21:05:37 +0700

    absen mysql

---
+++ alhazenacademy/intensive-bootcamp/mysql-batch-1/data.js
@@ -0,0 +1,14 @@
+let namelist = [
+    "Budi Purwantoni",
+    "Shanika Faida Azmi",
+    "Farhan Muwaffaq Dzaky",
+    "Adhia Dinda Sofia Afifah Masyhur",
+    "Muhamad Fauzi",
+    "idham kholid",
+    "Aqhbar Habib",
+    "Ismail",
+    "Rakha Ahmad Sahl Fawwaz",
+    "Ryan Virnanda"
+];
+
+namelist = [...new Set(namelist)].sort();
