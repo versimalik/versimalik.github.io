38901a6efcf4ad8a5b4d3906cf1c2c6e972a60c0
Reza Malik <rezamalikdrive@gmail.com>
2024-05-01 12:20:33 +0700

    data cicilan motor beat deluxe

---
+++ cicilan-beat-deluxe/data.js
@@ -0,0 +1,26 @@
+data = [
+    {
+        "nomor" : 1,
+        "tanggal" : "9 Februari 2024",
+        "jumlah" : 500000,
+        "bukti" : "https://drive.google.com/file/d/1t1jgTjo0y1GN9L4s1jI21W5fY76EmIDA/view"
+    },
+    {
+        "nomor" : 2,
+        "tanggal" : "24 Februari 2024",
+        "jumlah" : 500000,
+        "bukti" : "https://drive.google.com/file/d/1oHjYguhmKjNjVe2MzaA-9cEEKUZYp2Fh/view"
+    },
+    {
+        "nomor" : 3,
+        "tanggal" : "25 Maret 2024",
+        "jumlah" : 500000,
+        "bukti" : "https://drive.google.com/file/d/1Arpr-RyKF5GlyaBKdWsvWTbd-MwwQk4e/view"
+    },
+    {
+        "nomor" : 4,
+        "tanggal" : "25 April 2024",
+        "jumlah" : 500000,
+        "bukti" : "https://drive.google.com/file/d/14Ka1m85-_MagXsJ2ANrUOCQEnoU5Xslf/view"
+    }
+];
